69e74aa9f594c57ea4ca4fc077a898c3047e654b
jayden <jaydenshelton@yahoo.com>
2025-08-01 13:27:33 -0400

    Improved responsive design of About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -35,9 +35,9 @@ export const About = () => {
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                 </svg>
             </div>
-            <div className='flex md:flex-row xsm:max-md:flex-col md:justify-around xsm:justify-evenly  items-center relative h-full w-[90%]'>
+            <div className='flex items-center relative flex-col xl:flex-row  md:justify-around xsm:justify-evenly h-full w-[90%]'>
                 <motion.div
-                    className="flex items-center justify-center w-2/5 h-full md:w-5/12 md:h-3/5 xsm:w-full xsm:mt-10 sm:mt-20 xsm:max-md:h-2/6 lg:h-5/12"
+                    className="flex items-center justify-center w-2/5 h-full md:w-5/12 md:h-3/5 xsm:w-full  xsm:max-md:h-2/6 lg:h-5/12"
                     ref={imageRef}
                     initial="hidden"
                     animate={controlX}
@@ -51,7 +51,7 @@ export const About = () => {
                 </motion.div>
 
                 <motion.div
-                    className='flex flex-col items-start w-3/5 md:w-7/12 justify-evenly h-3/5 xsm:max-md:h-4/6 md:h-7/12 xsm:max-md:w-full'
+                    className='flex flex-col justify-between  items-start w-3/5 xsm:w-full md:max-lg:w-4/5 h-3/5'
                     ref={textRef}
                     initial="hidden"
                     animate={controlY}
@@ -61,19 +61,21 @@ export const About = () => {
                         visible: { opacity: 1, x: 0 }
                     }}
                 >
-                    <h1 className='w-full font-bold xsm:max-md:text-center xsm:text-md sm:text-lg lg:text-xlg'>Hey there! I'm <b className='text-primary'>Jayden</b></h1>
-                    <div className='flex flex-col font-normal justify-evenly xsm:h-1/2 sm:max-lg:h-4/5 xl:w-10/12 '>
+                    <div className='w-full xl:w-10/12 xl:text-center h-1/6 mt-3'>
+                        <h1 className='font-bold text-center xsm:text-md sm:text-lg lg:text-xlg'>Hey there! I'm <b className='text-primary'>Jayden</b></h1>
+                    </div>
+                    <div className='flex flex-col justify-evenly font-normal sm:max-md:w-full sm:max-md-p-8 xsm:h-1/2 sm:max-lg:h-4/5 xl:w-10/12 '>
                         <p>My goal as a software developer is to develop web apps and make challenging or even ordinary tasks easier. I am willing to achieve that goal alongside any team, for any client.</p>
                         <p className='xsm:max-lg:mb-3'>I have been tech-savvy since a young age. My dream is to work with development teams and make a significant impact in the tech industry. </p>
                     </div>
-                    <div className='flex flex-row items-center justify-between sm:w-5/6 sm:ms-auto sm:me-auto md:max-xl:w-full xl:w-4/5 xxl:w-full sm:h-1/5'>
+                    <div className='flex flex-row items-center justify-between w-full h-1/6 xl:w-10/12'>
                         <div className='about-icon-container'>
                             <AiFillPushpin className='about-icons' />
-                            <small className='about-details'>Location: Norwalk, Connecticut</small>
+                            <small className='about-details'><b>Location:</b> Norwalk, Connecticut</small>
                         </div>
                         <div className='about-icon-container'>
                             <FaGraduationCap className='about-icons' />
-                            <small className='about-details'>Education: University of Connecticut BA</small>
+                            <small className='about-details'><b>Education:</b> University of Connecticut BA</small>
                         </div>
                     </div>
                 </motion.div>
